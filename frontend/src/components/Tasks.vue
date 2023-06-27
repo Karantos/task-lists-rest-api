baea87056c1f487ff7595588e5bfdef19ea922cc
Karantos <jan.repar>
2023-06-27 20:17:18 +0200

    Revert "added status updates for tasks"
    This reverts commit 9f953db6550208fca450eac1cda7ba86998bb31c.

--- frontend/src/components/Tasks.vue
+++ frontend/src/components/Tasks.vue
@@ -21,17 +21,11 @@
 				<v-list-item
 					v-for="task in tasks"
 					:key="task.taskId"
-					@click="updateTaskStatus(task)"
+					:title="task.taskTitle"
+					@click="status(task)"
+					:class="{ 'text-decoration-line-through' : task.status }"
 				>   
-					
-					<v-list-item-title 
-							:class="{ 'text-decoration-line-through' : task.status }"
-					> 
-						{{ task.id }}
-						{{ task.taskTitle }}
-						{{ task.status }}
-					</v-list-item-title>
-					
+
 					<template v-slot:prepend>
 						<v-list-item-action start>
 							<v-checkbox-btn 
@@ -42,10 +36,9 @@
 					</template>
 
 					<template v-slot:append>
-						<v-btn color="grey" icon="mdi-pencil-outline" variant="text" @click="updateTask(task)"></v-btn>
+						<v-btn color="grey" icon="mdi-pencil-outline" variant="text" @click="updateTask"></v-btn>
 						<v-btn color="grey" icon="mdi-delete-outline" variant="text" @click="deleteTask(task.id)"></v-btn>
 					</template>
-
 
 				</v-list-item>
 			</v-list>
@@ -58,10 +51,10 @@ import ListsAppDataService from '@/services/ListsAppDataService'
 
 export default {
 	name: "tasks",
+	message: "",
 	data() {
 		return {
 			tasks: [],
-			message: "",
 			task: {
 				id: null,
 				title: "",
@@ -81,8 +74,8 @@ export default {
 			console.log(addTask)
 		},
 
-		getTask(taskId) {
-			ListsAppDataService.getTask(taskId)
+		getTask(id) {
+			ListsAppDataService.getTask(id)
 				.then(response => {
 					this.tasks = response.data;
 					console.log(response.data);
@@ -113,11 +106,11 @@ export default {
 				});
 		},
 
-		deleteTask(id) {
-			ListsAppDataService.deleteTask(id)
+		deleteTask(taskId) {
+			ListsAppDataService.deleteTask(taskId)
 				.then(response => {
 					console.log(response.data);
-					const taskIndex = this.tasks.findIndex(task => task.id === id) // finds index of array(tasks) of a task which id == id of the task to delete
+					const taskIndex = this.tasks.findIndex(task => task.id === taskId) // finds index of array(tasks) of a task which id == id of the task to delete
 					this.tasks.splice(taskIndex, 1) // removes 1 element from array index that is defined by taskIndex
 				})
 				.catch(error => {
@@ -125,31 +118,21 @@ export default {
 				});
 		},
 
-		updateTaskStatus(task) {
-			let data = task;
-			task.status = !task.status;
+		updateTask() {
+			let task = {
+				taskId: this.task.id,
+				taskTitle: this.task.title,
+				taskStatus: this.task.status,
+				tasksList: {
+					listId: this.$route.params.id
+				}
+			};
 
-			ListsAppDataService.updateTask(data)
+			ListsAppDataService.updateTask(task)
 				.then(response => {
 					task.status = !task.status;
 					console.log(response.data);
-					this.$router.go(0);
-				})
-				.catch(error => {
-					console.log(error);
-				});
-		},
-
-		updateTask(task) {
-			let data = task;
-			data = {
-				taskTitle: this.title
-			}
-
-			ListsAppDataService.updateTask(data)
-				.then(response => {
-					console.log(response.data);
-					this.$router.go(0);
+					this.message = "You have successfully changed task name!"
 				})
 				.catch(error => {
 					console.log(error);
